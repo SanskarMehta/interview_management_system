06a96a21ed076678649ee064188a907f126085b5
Sanskar Mehta <sanskar.inexture@gmail.com>
2022-07-11 16:50:57 +0530

    "Final Version 11-07-2022"

--- interview_management_system/settings.py
+++ interview_management_system/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)xh0gh7+04*d_f-a=gle7_d=ed&na-j6$7)#rb8f(*mc$9dr@9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['interview-management-final']
 
 # Application definition
 
