c93d06a4049dd0804b75e0b3ea92c01840ccfa60
Sanskar <sanskar.inexture@gmail.com>
2023-03-06 10:16:18 +0530

    Commiting updated features

--- interview_management_system/urls.py
+++ interview_management_system/urls.py
@@ -20,8 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('user_login.urls')),
-    path('administration/',include('administration.urls')),
+    path('', include('user_login.urls')),
+    path('administration/', include('administration.urls')),
 ]
 
 if settings.DEBUG:
